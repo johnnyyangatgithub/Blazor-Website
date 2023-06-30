ab3302b34018fd949a8a2ba700dc08c5ebd46d33
Johnny <yuhangyang74@gmail.com>
2023-06-30 12:26:01 +0800

    Initial commit

---
+++ BlazorEcommerceWebsite/Shared/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace BlazorEcommerceWebsite.Shared
+{
+	public class Product
+	{
+        public int ID { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+}
+
