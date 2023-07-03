dad90a86619e8a7771ca73ba1aead078a1d5828d
Johnny <yuhangyang74@gmail.com>
2023-07-03 10:22:02 +0800

    Added SwaggerUI

--- BlazorEcommerceWebsite/Server/Controllers/ProductController.cs
+++ BlazorEcommerceWebsite/Server/Controllers/ProductController.cs
@@ -39,7 +39,7 @@ namespace BlazorEcommerceWebsite.Server.Controllers
         };
 
         [HttpGet]
-        public async Task<IActionResult> GetProduct()
+        public async Task<ActionResult<List<Product>>> GetProduct()
         {
             return Ok(Products);
         }
