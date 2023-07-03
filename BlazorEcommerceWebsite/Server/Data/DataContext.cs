27104804d313b8e292e2d425d318ecc783c34793
Johnny <yuhangyang74@gmail.com>
2023-07-03 12:16:48 +0800

    -Add the DataContext
    -Register the DbContext and configure SqlServer

---
+++ BlazorEcommerceWebsite/Server/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BlazorEcommerceWebsite.Server.Data
+{
+	public class DataContext : DbContext
+	{
+		public DataContext(DbContextOptions<DataContext> options) : base(options)
+		{
+
+		}
+
+		public DbSet<Product> Products { get; set; }
+	}
+}
+
