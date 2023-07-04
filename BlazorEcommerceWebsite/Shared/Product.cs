2ad96f023def08c3cba53265684f92776f98b5ff
Johnny <yuhangyang74@gmail.com>
2023-07-04 09:32:03 +0800

    -Implemented the migration with Entity Framework Core
    -Get connected with SQL database.

--- BlazorEcommerceWebsite/Shared/Product.cs
+++ BlazorEcommerceWebsite/Shared/Product.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace BlazorEcommerceWebsite.Shared
 {
 	public class Product
 	{
         public int ID { get; set; }
+
         public string Title { get; set; } = string.Empty;
+
         public string Description { get; set; } = string.Empty;
+
         public string ImageUrl { get; set; } = string.Empty;
+
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
     }
 }
