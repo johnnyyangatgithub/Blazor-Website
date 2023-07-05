f419de10dbe43dce0fc429093eb3c353a3cdb9ba
Johnny <yuhangyang74@gmail.com>
2023-07-05 11:26:03 +0800

    Created a ProductDetails component

--- BlazorEcommerceWebsite/Server/Data/DataContext.cs
+++ BlazorEcommerceWebsite/Server/Data/DataContext.cs
@@ -15,7 +15,7 @@ namespace BlazorEcommerceWebsite.Server.Data
                 new Product
                 {
                     ID = 1,
-                    Title = "The hitchhiker's guide to the galaxy d",
+                    Title = "The hitchhiker's guide to the galaxy",
                     Description = "The Hitchhiker's Guide to the Galaxy is a comedy science fiction franchise created by Douglas Adams. Originally a 1978 radio comedy broadcast on BBC Radio 4, it was later adapted to other formats, including novels, stage shows, comic books, a 1981 TV series, a 1984 text-based computer game, and 2005 feature film.",
                     ImageUrl = "https://upload.wikimedia.org/wikipedia/en/b/bd/H2G2_UK_front_cover.jpg",
                     Price = 9.99m
