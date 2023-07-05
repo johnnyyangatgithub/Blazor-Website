733edca89914931ef5bb74b63025c0cd040e6944
Johnny <yuhangyang74@gmail.com>
2023-07-05 14:39:18 +0800

    Added Category seeding and migration

---
+++ BlazorEcommerceWebsite/Shared/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace BlazorEcommerceWebsite.Shared
+{
+    public class Category
+    {
+        public Category()
+        {
+        }
+
+        public int Id {
+            get; set;
+        }
+
+        public string Name {
+            get; set;
+        } = string.Empty;
+
+        public string Url {
+            get; set;
+        } = string.Empty;
+    }
+}
+
--- BlazorEcommerceWebsite/Shared/Product.cs
+++ BlazorEcommerceWebsite/Shared/Product.cs
@@ -15,6 +15,9 @@ namespace BlazorEcommerceWebsite.Shared
 
         [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
+
+        public Category? Category { get; set; }
+        public int CategoryId { get; set; }
     }
 }
 
