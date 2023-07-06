7bef638b61c120b9ba5e39b3fa3d6715228ae6ab
Johnny <yuhangyang74@gmail.com>
2023-07-06 14:40:39 +0800

    Introduced Product Search Feature

--- BlazorEcommerceWebsite/Server/Services/ProductService/IProductService.cs
+++ BlazorEcommerceWebsite/Server/Services/ProductService/IProductService.cs
@@ -6,6 +6,7 @@ namespace BlazorEcommerceWebsite.Server.Services.ProductService
 		Task<ServiceResponse<List<Product>>> GetProductsAsync();
 		Task<ServiceResponse<Product>> GetProductAsync(int productId);
         Task<ServiceResponse<List<Product>>> GetProductsByCategory (string categoryUrl);
+		Task<ServiceResponse<List<Product>>> SearchProducts ( string searchText );
     }
 }
 
