179d7481e193106cb035dd6c42e6ccf37414424e
Johnny <yuhangyang74@gmail.com>
2023-07-06 12:39:21 +0800

    Get Product by Category on the Client

--- BlazorEcommerceWebsite/Client/Services/ProductService/IProductService.cs
+++ BlazorEcommerceWebsite/Client/Services/ProductService/IProductService.cs
@@ -3,8 +3,9 @@ namespace BlazorEcommerceWebsite.Client.Services.ProductService
 {
 	public interface IProductService
 	{
+		event Action ProductsChanged;
 		List<Product> Products { get; set; }
-		Task GetProducts();
+		Task GetProducts(string? categoryUrl = null);
 		Task<ServiceResponse<Product>> GetProduct(int productId);
 	}
 }
