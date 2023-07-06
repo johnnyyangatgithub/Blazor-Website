728c398b5fc89099b689aa74ab4a9019854140f4
Johnny <yuhangyang74@gmail.com>
2023-07-06 15:12:47 +0800

    Implement a Search via URL in the main page component

--- BlazorEcommerceWebsite/Client/Services/ProductService/IProductService.cs
+++ BlazorEcommerceWebsite/Client/Services/ProductService/IProductService.cs
@@ -5,8 +5,11 @@ namespace BlazorEcommerceWebsite.Client.Services.ProductService
 	{
 		event Action ProductsChanged;
 		List<Product> Products { get; set; }
+		string Message { get; set; }
 		Task GetProducts(string? categoryUrl = null);
 		Task<ServiceResponse<Product>> GetProduct(int productId);
+		Task SearchProducts ( string searchText );
+		Task<List<string>> GetProductSearchSuggestions ( string searchText );
 	}
 }
 
--- BlazorEcommerceWebsite/Client/Services/ProductService/ProductService.cs
+++ BlazorEcommerceWebsite/Client/Services/ProductService/ProductService.cs
@@ -11,6 +11,7 @@ namespace BlazorEcommerceWebsite.Client.Services.ProductService
         }
 
         public List<Product> Products { get; set; } = new List<Product>();
+        public string Message { get; set; } = "Loading Products...";
 
         public event Action ProductsChanged;
 
@@ -29,6 +30,28 @@ namespace BlazorEcommerceWebsite.Client.Services.ProductService
                 Products = result.Data;
 
             ProductsChanged.Invoke(); 
+        }
+
+        public async Task<List<string>> GetProductSearchSuggestions(string searchText)
+        {
+            var result = await _http
+                .GetFromJsonAsync<ServiceResponse<List<string>>>( $"api/product/searchsuggestions/{searchText}" );
+
+            return result.Data;
+        }
+
+        public async Task SearchProducts(string searchText)
+        {
+            var result = await _http
+                .GetFromJsonAsync<ServiceResponse<List<Product>>>( $"api/product/search/{searchText}" );
+
+            if(result != null && result.Data != null)
+            {
+                Products = result.Data;
+            }
+            if ( Products.Count == 0 ) Message = "No prducts found.";
+
+            ProductsChanged?.Invoke();
         }
     }
 }
