a854213efd4e9228d38bdaffbea838934888da82
Johnny <yuhangyang74@gmail.com>
2023-07-08 07:18:33 +0800

    Introducing Pagination and DTO
    Implement Product Search Pagination on the server

--- BlazorEcommerceWebsite/Server/Controllers/ProductController.cs
+++ BlazorEcommerceWebsite/Server/Controllers/ProductController.cs
@@ -42,10 +42,10 @@ namespace BlazorEcommerceWebsite.Server.Controllers
             return Ok( result );
         }
 
-        [HttpGet( "search/{searchText}" )]
-        public async Task<ActionResult<ServiceResponse<List<Product>>>> SearchProducts ( string searchText)
+        [HttpGet( "search/{searchText}/{page}" )]
+        public async Task<ActionResult<ServiceResponse<ProductSearchResultDTO>>> SearchProducts ( string searchText, int page)
         {
-            var result = await _productService.SearchProducts( searchText );
+            var result = await _productService.SearchProducts( searchText, page );
             return Ok( result );
         }
 
---
+++ BlazorEcommerceWebsite/Shared/ProductSearchResultDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace BlazorEcommerceWebsite.Shared
+{
+    public class ProductSearchResultDTO
+    {
+        public List<Product> Products { get; set; } = new List<Product>();
+        public int Pages { get; set; }
+        public int CurrentPage { get; set; }
+    }
+}
+
