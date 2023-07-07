06968b05d1ab7bd28b3d38525cca2163717413b0
Johnny <yuhangyang74@gmail.com>
2023-07-07 15:27:18 +0800

    Introduced Featured Products

---
+++ BlazorEcommerceWebsite/Server/Migrations/20230707072218_FeaturedProducts.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorEcommerceWebsite.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class FeaturedProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Featured",
+                table: "Products",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ID",
+                keyValue: 1,
+                column: "Featured",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ID",
+                keyValue: 2,
+                column: "Featured",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ID",
+                keyValue: 3,
+                column: "Featured",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ID",
+                keyValue: 4,
+                column: "Featured",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ID",
+                keyValue: 5,
+                column: "Featured",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ID",
+                keyValue: 6,
+                column: "Featured",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ID",
+                keyValue: 7,
+                column: "Featured",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ID",
+                keyValue: 8,
+                column: "Featured",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ID",
+                keyValue: 9,
+                column: "Featured",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ID",
+                keyValue: 10,
+                column: "Featured",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ID",
+                keyValue: 11,
+                column: "Featured",
+                value: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Featured",
+                table: "Products");
+        }
+    }
+}
--- BlazorEcommerceWebsite/Shared/Product.cs
+++ BlazorEcommerceWebsite/Shared/Product.cs
@@ -16,6 +16,8 @@ namespace BlazorEcommerceWebsite.Shared
         public Category? Category { get; set; }
         public int CategoryId { get; set; }
 
+        public bool Featured { get; set; } = false;
+
         public List<ProductVariant> Variants { get; set; } = new List<ProductVariant>();
     }
 }
