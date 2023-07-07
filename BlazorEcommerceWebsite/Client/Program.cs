450f8fe862aafba31e5d178ed2b8228e995ce085
Johnny <yuhangyang74@gmail.com>
2023-07-08 07:51:21 +0800

    Introduced the Local Storage package into the project

--- BlazorEcommerceWebsite/Client/Program.cs
+++ BlazorEcommerceWebsite/Client/Program.cs
@@ -5,11 +5,13 @@ global using BlazorEcommerceWebsite.Client.Services.CategoryService;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorEcommerceWebsite.Client;
+using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
