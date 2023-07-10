9b3037e778eff0ef44e0dcb5ff0c221bfcd9c7a9
Johnny <yuhangyang74@gmail.com>
2023-07-10 15:07:45 +0800

    Added an UserRegister model

---
+++ BlazorEcommerceWebsite/Shared/UserRegister.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace BlazorEcommerceWebsite.Shared
+{
+    public class UserRegister
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
+
