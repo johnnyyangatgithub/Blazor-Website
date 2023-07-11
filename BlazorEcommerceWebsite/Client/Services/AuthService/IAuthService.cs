8bf40443fb291727c273a0d06a211150e3ea7560
Johnny <yuhangyang74@gmail.com>
2023-07-11 15:00:36 +0800

    Implement the login on the client

--- BlazorEcommerceWebsite/Client/Services/AuthService/IAuthService.cs
+++ BlazorEcommerceWebsite/Client/Services/AuthService/IAuthService.cs
@@ -4,6 +4,7 @@ namespace BlazorEcommerceWebsite.Client.Services.AuthService
     public interface IAuthService
     {
         Task<ServiceResponse<int>> Register ( UserRegister request );
+        Task<ServiceResponse<string>> Login ( UserLogin request );
     }
 }
 
--- BlazorEcommerceWebsite/Server/Services/AuthService/AuthService.cs
+++ BlazorEcommerceWebsite/Server/Services/AuthService/AuthService.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Security.Cryptography;
 using Azure.Core;
+using Microsoft.IdentityModel.Tokens;
 using static System.Net.WebRequestMethods;
 
 namespace BlazorEcommerceWebsite.Server.Services.AuthService
@@ -8,9 +11,11 @@ namespace BlazorEcommerceWebsite.Server.Services.AuthService
     public class AuthService : IAuthService
     {
         private readonly DataContext _context;
-        public AuthService (DataContext context)
+        private readonly IConfiguration _configuration;
+        public AuthService (DataContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
         }
 
         public async Task<ServiceResponse<string>> Login(string email, string password)
@@ -29,7 +34,7 @@ namespace BlazorEcommerceWebsite.Server.Services.AuthService
             }
             else
             {
-                response.Data = "token";
+                response.Data = CreateToken(user);
             }
 
             return response;
@@ -85,6 +90,28 @@ namespace BlazorEcommerceWebsite.Server.Services.AuthService
                 var computedHash = hmac.ComputeHash( System.Text.Encoding.UTF8.GetBytes( password ) );
                 return computedHash.SequenceEqual( passwordHash );
             }
+        }
+
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Email)
+            };
+
+            var key = new SymmetricSecurityKey( System.Text.Encoding.UTF8.GetBytes( _configuration.GetSection( "AppSettings:Token" ).Value ));
+            var creds = new SigningCredentials( key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays( 1 ),
+                signingCredentials: creds );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken( token );
+
+            return jwt;
+
         }
     }
 }
