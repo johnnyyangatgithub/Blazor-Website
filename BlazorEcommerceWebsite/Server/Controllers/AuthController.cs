d575c3ba033959c61f854aa6825bfb815d3853be
Johnny <yuhangyang74@gmail.com>
2023-07-11 12:05:14 +0800

    Added the AuthController

---
+++ BlazorEcommerceWebsite/Server/Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorEcommerceWebsite.Server.Controllers
+{
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+        public AuthController (IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<ServiceResponse<int>>> Register(UserRegister request)
+        {
+            var response = await _authService.Register(
+                new User
+                {
+                    Email = request.Email
+                }, request.Password );
+
+            if(!response.Success)
+            {
+                return BadRequest( response );
+            }
+
+            return Ok( response );
+        }
+    }
+}
+
