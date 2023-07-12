d4e0cf41457cf59a8c956ff14654ff5c303a9927
Johnny <yuhangyang74@gmail.com>
2023-07-12 11:49:27 +0800

    Implemented the change password function on the server

--- BlazorEcommerceWebsite/Server/Services/AuthService/IAuthService.cs
+++ BlazorEcommerceWebsite/Server/Services/AuthService/IAuthService.cs
@@ -6,6 +6,7 @@ namespace BlazorEcommerceWebsite.Server.Services.AuthService
         Task<ServiceResponse<int>> Register ( User user, string password );
         Task<bool> UserExists ( string email );
         Task<ServiceResponse<string>> Login ( string email, string password );
+        Task<ServiceResponse<bool>> ChangePassword ( int userId, string newPassword );
     }
 }
 
