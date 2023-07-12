599473983b64d6269703e4e67d9a630152100d8c
Johnny <yuhangyang74@gmail.com>
2023-07-12 11:29:18 +0800

    Added the UserChangePassword model

---
+++ BlazorEcommerceWebsite/Shared/UserChangePassword.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorEcommerceWebsite.Shared
+{
+    public class UserChangePassword
+    {
+        [Required, StringLength(100, MinimumLength = 6)]
+        public string Password { get; set; } = string.Empty;
+
+        [Compare("Password", ErrorMessage = "The passwords do not match.")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
+
