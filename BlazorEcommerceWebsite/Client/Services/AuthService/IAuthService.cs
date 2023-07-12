79cb240a401aeb7c7fd302efbd5834a528f0ac74
Johnny <yuhangyang74@gmail.com>
2023-07-12 12:09:05 +0800

    Implemented changing password on the client

--- BlazorEcommerceWebsite/Client/Services/AuthService/IAuthService.cs
+++ BlazorEcommerceWebsite/Client/Services/AuthService/IAuthService.cs
@@ -5,6 +5,7 @@ namespace BlazorEcommerceWebsite.Client.Services.AuthService
     {
         Task<ServiceResponse<int>> Register ( UserRegister request );
         Task<ServiceResponse<string>> Login ( UserLogin request );
+        Task<ServiceResponse<bool>> ChangePassword ( UserChangePassword request);
     }
 }
 
