ada0cff5e15dff83d9691eb0272d07a421405a5e
Johnny <yuhangyang74@gmail.com>
2023-07-13 14:31:03 +0800

    Implemented the Order model and update the database

--- BlazorEcommerceWebsite/Server/Data/DataContext.cs
+++ BlazorEcommerceWebsite/Server/Data/DataContext.cs
@@ -17,6 +17,9 @@ namespace BlazorEcommerceWebsite.Server.Data
             modelBuilder.Entity<ProductVariant>()
                 .HasKey( p => new { p.ProductId, p.ProductTypeId } );
 
+            modelBuilder.Entity<OrderItem>()
+                .HasKey( oi => new { oi.OrderId, oi.ProductId, oi.ProductTypeId } );
+
             modelBuilder.Entity<ProductType>().HasData(
                     new ProductType { Id = 1, Name = "Default" },
                     new ProductType { Id = 2, Name = "Paperback" },
@@ -268,6 +271,8 @@ namespace BlazorEcommerceWebsite.Server.Data
         public DbSet<ProductVariant> ProductVariants { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
     }
 }
 
