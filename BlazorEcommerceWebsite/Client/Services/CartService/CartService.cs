05c5000a09b5f8c5eb673992a6892c5d1059e5a7
Johnny <yuhangyang74@gmail.com>
2023-07-13 11:34:56 +0800

    Refactor the CartService on the client side

--- BlazorEcommerceWebsite/Client/Services/CartService/CartService.cs
+++ BlazorEcommerceWebsite/Client/Services/CartService/CartService.cs
@@ -57,23 +57,24 @@ namespace BlazorEcommerceWebsite.Client.Services.CartService
             await GetCartItemsCount();
         }
 
-        public async Task<List<CartItem>> GetCartItems()
-        {
-            await GetCartItemsCount();
-            var cart = await _localStorage.GetItemAsync<List<CartItem>>( "cart" );
-            if ( cart == null )
-            {
-                cart = new List<CartItem>();
-            }
-            return cart;
-        }
-
         public async Task<List<CartProductResponseDTO>> GetCartProducts()
         {
-            var cartItems = await _localStorage.GetItemAsync<List<CartItem>>( "cart" );
-            var response = await _http.PostAsJsonAsync( "api/cart/products", cartItems );
-            var cartProducts = await response.Content.ReadFromJsonAsync<ServiceResponse<List<CartProductResponseDTO>>>();
-            return cartProducts.Data;
+            if(await IsUserAuthenticated())
+            {
+                var response = await _http.GetFromJsonAsync<ServiceResponse<List<CartProductResponseDTO>>>( "api/cart" );
+                return response.Data;
+            }
+            else
+            {
+                var cartItems = await _localStorage.GetItemAsync<List<CartItem>>( "cart" );
+                if(cartItems == null)
+                {
+                    return new List<CartProductResponseDTO>();
+                }
+                var response = await _http.PostAsJsonAsync( "api/cart/products", cartItems );
+                var cartProducts = await response.Content.ReadFromJsonAsync<ServiceResponse<List<CartProductResponseDTO>>>();
+                return cartProducts.Data;
+            }
         }
 
         public async Task RemoveProductFromCart(int productId, int productTypeId)
--- BlazorEcommerceWebsite/Client/Services/CartService/ICartService.cs
+++ BlazorEcommerceWebsite/Client/Services/CartService/ICartService.cs
@@ -5,7 +5,6 @@ namespace BlazorEcommerceWebsite.Client.Services.CartService
     {
         event Action OnChange;
         Task AddToCart ( CartItem cartItem );
-        Task<List<CartItem>> GetCartItems();
         Task<List<CartProductResponseDTO>> GetCartProducts();
         Task RemoveProductFromCart ( int productId, int productTypeId );
         Task UpdateQuantity ( CartProductResponseDTO product );
