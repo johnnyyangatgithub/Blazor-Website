c2d3d2a203a6cb34bf04a89efa82571fd6a4f53b
Johnny <yuhangyang74@gmail.com>
2023-07-14 09:34:20 +0800

    Implement placing an order on the client

--- BlazorEcommerceWebsite/Client/Program.cs
+++ BlazorEcommerceWebsite/Client/Program.cs
@@ -3,12 +3,13 @@ global using System.Net.Http.Json;
 global using BlazorEcommerceWebsite.Client.Services.ProductService;
 global using BlazorEcommerceWebsite.Client.Services.CategoryService;
 global using Microsoft.AspNetCore.Components.Authorization;
+global using BlazorEcommerceWebsite.Client.Services.CartService;
+global using BlazorEcommerceWebsite.Client.Services.AuthService;
+global using BlazorEcommerceWebsite.Client.Services.OrderService;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorEcommerceWebsite.Client;
 using Blazored.LocalStorage;
-using BlazorEcommerceWebsite.Client.Services.CartService;
-using BlazorEcommerceWebsite.Client.Services.AuthService;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -20,6 +21,7 @@ builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<ICartService, CartService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IOrderService, OrderService>();
 //Related to the Microsoft.AspNetCore.Components.Authorization package
 builder.Services.AddOptions();
 builder.Services.AddAuthorizationCore();
---
+++ BlazorEcommerceWebsite/Client/Services/OrderService/IOrderService.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace BlazorEcommerceWebsite.Client.Services.OrderService
+{
+    public interface IOrderService
+    {
+        Task PlaceOrder ();
+    }
+}
+
