194a27d62ca5a96b37f87e368d3152f4a386b7aa
Johnny <yuhangyang74@gmail.com>
2023-07-19 16:17:38 +0800

    Added more properties to the category model

---
+++ BlazorEcommerceWebsite/Server/Migrations/20230719081015_CategoryFlags.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorEcommerceWebsite.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class CategoryFlags : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "Categories",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Visbile",
+                table: "Categories",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Deleted", "Visbile" },
+                values: new object[] { false, false });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Deleted", "Visbile" },
+                values: new object[] { false, false });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "Deleted", "Visbile" },
+                values: new object[] { false, false });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "Visbile",
+                table: "Categories");
+        }
+    }
+}
--- BlazorEcommerceWebsite/Shared/Category.cs
+++ BlazorEcommerceWebsite/Shared/Category.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace BlazorEcommerceWebsite.Shared
 {
     public class Category
@@ -7,17 +9,15 @@ namespace BlazorEcommerceWebsite.Shared
         {
         }
 
-        public int Id {
-            get; set;
-        }
-
-        public string Name {
-            get; set;
-        } = string.Empty;
-
-        public string Url {
-            get; set;
-        } = string.Empty;
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Url { get; set; } = string.Empty;
+        public bool Visbile { get; set; } = false;
+        public bool Deleted { get; set; } = false;
+        [NotMapped]
+        public bool Editing { get; set; } = false;
+        [NotMapped]
+        public bool IsNew { get; set; } = false;
     }
 }
 
