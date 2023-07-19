124855766e01d846145ee6445c8e788796b77f2c
Johnny <yuhangyang74@gmail.com>
2023-07-19 13:13:43 +0800

    Add a Rile to the User model and to the token in the AuthService to get admin dashboard prepared

--- BlazorEcommerceWebsite/Server/Services/AuthService/AuthService.cs
+++ BlazorEcommerceWebsite/Server/Services/AuthService/AuthService.cs
@@ -104,7 +104,8 @@ namespace BlazorEcommerceWebsite.Server.Services.AuthService
             List<Claim> claims = new List<Claim>
             {
                 new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
-                new Claim(ClaimTypes.Name, user.Email)
+                new Claim(ClaimTypes.Name, user.Email),
+                new Claim(ClaimTypes.Role, user.Role)
             };
 
             var key = new SymmetricSecurityKey( System.Text.Encoding.UTF8.GetBytes( _configuration.GetSection( "AppSettings:Token" ).Value ) );
--- BlazorEcommerceWebsite/Shared/User.cs
+++ BlazorEcommerceWebsite/Shared/User.cs
@@ -9,6 +9,7 @@ namespace BlazorEcommerceWebsite.Shared
         public byte[] PasswordSalt { get; set; }
         public DateTime DateCreated { get; set; } = DateTime.Now;
         public Address Address { get; set; }
+        public string Role { get; set; } = "Customer";
     }
 }
 
